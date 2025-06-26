a11ba80ba660241191da1e83f0dc711de4abd0c4
Ablebil <hafizhfaiq07@gmail.com>
2025-06-26 15:06:48 +0700

    feat: add authentication middleware for protected endpoints

---
+++ internal/middleware/authentication.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	"fmt"
+	"strings"
+
+	res "github.com/Ablebil/sea-catering-be/internal/infra/response"
+	"github.com/gofiber/fiber/v2"
+)
+
+func (m *Middleware) Authentication(ctx *fiber.Ctx) error {
+	authHeader := ctx.Get("Authorization")
+	if authHeader == "" {
+		return res.ErrUnauthorized(res.MissingAccessToken)
+	}
+
+	parts := strings.Fields(authHeader)
+	if len(parts) != 2 || strings.ToLower(parts[0]) != "bearer" {
+		return res.ErrUnauthorized(res.InvalidOrMissingBearerToken)
+	}
+
+	userID, name, email, err := m.jwt.VerifyAccessToken(parts[1])
+	if err != nil {
+		fmt.Println("Detailed Error:", err)
+		return res.ErrUnauthorized(res.InvalidAccessToken)
+	}
+
+	ctx.Locals("userID", userID)
+	ctx.Locals("name", name)
+	ctx.Locals("email", email)
+
+	return ctx.Next()
+}
---
+++ internal/middleware/middleware.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"github.com/Ablebil/sea-catering-be/internal/infra/jwt"
+	"github.com/gofiber/fiber/v2"
+)
+
+type MiddlewareItf interface {
+	Authentication(ctx *fiber.Ctx) error
+}
+
+type Middleware struct {
+	jwt jwt.JWTItf
+}
+
+func NewMiddleware(jwt jwt.JWTItf) MiddlewareItf {
+	return &Middleware{
+		jwt: jwt,
+	}
+}
